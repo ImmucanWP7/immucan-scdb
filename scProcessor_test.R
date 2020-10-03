93821bd86433579570ff67ab410b95c949d718e7
GLANL <jordi.camps@bayer.com>
2020-10-03 13:15:20 +0200

    No batch variable necessary anymore

--- scProcessor_test.R
+++ scProcessor_test.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-batch = args[1] #batch variable in the metadata slot
-normalized = args[2] #Boolean to indicate if data is already normalized
+#batch = args[1] #batch variable in the metadata slot
+normalized = args[1] #Boolean to indicate if data is already normalized
 QC_feature_min = 200 #Minimal features threshold
 QC_mt_max = 15 #Maximum mitochondrial content threshold
 pca_dims = 30 #Amount of PCA dimensions to use
@@ -22,25 +22,30 @@ ifelse(!dir.exists("out"), dir.create("out"), FALSE)
 # QC
 
 seurat <- readRDS(object_path)
-cells_before_QC <- ncol(seurat)
 seurat[["percent.mt"]] <- PercentageFeatureSet(seurat, pattern = "^MT-")
 
-p1 <- AugmentPlot(VlnPlot(seurat, features = "nFeature_RNA", pt.size = 0.1, group.by = batch, log = TRUE)) + 
-  NoLegend() +
-  scale_y_log10("Genes", expand = c(0,0)) + 
-  geom_hline(yintercept = QC_feature_min, color = "red") + 
-  theme(axis.title.x = element_blank(), plot.title = element_blank(), axis.title.y = element_text(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
-
-p2 <- AugmentPlot(VlnPlot(seurat, features = "nCount_RNA", pt.size = 0.1, group.by = batch, log = TRUE)) + 
-  NoLegend() + 
-  scale_y_log10("Counts", expand = c(0,0)) + 
-  theme(axis.title.x = element_blank(), plot.title = element_blank(), axis.title.y = element_text(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
-
-p3 <- AugmentPlot(VlnPlot(seurat, features = "percent.mt", pt.size = 0.1, group.by = batch)) + 
-  NoLegend() +
-  geom_hline(yintercept = QC_mt_max, color = "red") + 
-  scale_y_continuous("Mito", expand = c(0,0)) +
-  theme(axis.title.x = element_blank(), plot.title = element_blank(), axis.title.y = element_text())
+for (i in colnames(seurat@meta.data)) {
+  if (is.character(seurat@meta.data[[i]]) == TRUE) {
+    if (length(unique(seurat@meta.data[[i]])) > 1) {
+	p1 <- AugmentPlot(VlnPlot(seurat, features = "nFeature_RNA", pt.size = 0.1, group.by = i, log = TRUE)) + 
+  	 	NoLegend() +
+  	  	scale_y_log10("Genes", expand = c(0,0)) + 
+  	 	geom_hline(yintercept = QC_feature_min, color = "red") + 
+  	 	theme(axis.title.x = element_blank(), plot.title = element_blank(), axis.title.y = element_text(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
+	p2 <- AugmentPlot(VlnPlot(seurat, features = "nCount_RNA", pt.size = 0.1, group.by = i, log = TRUE)) + 
+  		NoLegend() + 
+  		scale_y_log10("Counts", expand = c(0,0)) + 
+  		theme(axis.title.x = element_blank(), plot.title = element_blank(), axis.title.y = element_text(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
+	p3 <- AugmentPlot(VlnPlot(seurat, features = "percent.mt", pt.size = 0.1, group.by = i)) + 
+  		NoLegend() +
+  		geom_hline(yintercept = QC_mt_max, color = "red") + 
+  		scale_y_continuous("Mito", expand = c(0,0)) +
+  		theme(axis.title.x = element_blank(), plot.title = element_blank(), axis.title.y = element_text())
+	p <- p1 / p2 / p3
+	ggsave(plot = p, filename = paste0("temp/QC", i, ".png"))
+	}
+    }
+}
 
 seurat <- subset(seurat, subset = nFeature_RNA > QC_feature_min & percent.mt < QC_mt_max)
 
@@ -50,11 +55,23 @@ if (normalized == FALSE) {
   seurat <- Seurat::NormalizeData(seurat, verbose = TRUE)
 }
 seurat <- seurat %>% 
-  FindVariableFeatures(selection.method = "vst", nfeatures = features_var, verbose=TRUE) %>% 
+  FindVariableFeatures(selection.method = "vst", nfeatures = features_var, verbose = TRUE) %>% 
   ScaleData(verbose = TRUE) %>% 
   RunPCA(pc.genes = seurat@var.genes, npcs = pca_dims+20, verbose = TRUE) %>%
   RunUMAP(dims = 1:pca_dims, a = .5, b = 1.2, verbose = TRUE)
 
 p4 <- ElbowPlot(seurat, ndims = pca_dims+20) + geom_vline(xintercept = pca_dims, color = "red") + ylab("STDEV PCA") + theme(axis.title.x = element_blank())
-p <- p4 / p1 / p2 / p3
-ggsave(plot = p, filename = "out/QC.png")
+ggsave(plot = p4, filename = "temp/Elbow.png")
+
+# Plotting
+
+for (i in colnames(seurat@meta.data)) {
+  if (is.character(seurat@meta.data[[i]]) == TRUE) {
+    if (length(unique(seurat@meta.data[[i]])) > 1) {
+      p1 <- DimPlot(seurat, reduction = "pca", pt.size = 1, group.by = i, label = TRUE)
+      p2 <- DimPlot(seurat, reduction = "umap", pt.size = 1, group.by = i, label = TRUE)
+      p <- p1 / p2
+      ggsave(plot = p, filename = paste0("temp/", i, ".png"))
+    }
+  }
+}

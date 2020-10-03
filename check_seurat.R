0c4877adfccf53793058d7d70c8f16ee630abd15
GLANL <jordi.camps@bayer.com>
2020-10-03 15:26:57 +0200

    create raw.rds and data.rds

--- check_seurat.R
+++ check_seurat.R
@@ -2,9 +2,15 @@
 args = commandArgs(trailingOnly=TRUE)
 seurat_obj = args[1] #path of seurat object
 
+dir <- getwd()
+setwd(dir)
+ifelse(!dir.exists("temp"), dir.create("temp"), FALSE)
+ifelse(!dir.exists("out"), dir.create("out"), FALSE)
+
 library(Seurat)
 
-seurat <- readRDS(seurat_obj)
+seurat_temp <- readRDS(seurat_obj)
+seurat <- CreateSeuratObject(counts = seurat_temp[["RNA"]]@counts, meta.data = seurat_temp@meta.data, min.cells = 10, min.features = 200)
 
 print(paste0("CELL_ID = ", colnames(seurat)[1]))
 print(paste0("CELL_NUMBER = ", ncol(seurat)))
@@ -18,11 +24,17 @@ if (sum(colnames(seurat) == rownames(seurat@meta.data)) == ncol(seurat)) {
 }
 
 gapdh <- grepl("GAPDH|gapdh", rownames(seurat))
+data <- list()
 if (sum(grepl("//.", seurat[["RNA"]]@counts[gapdh, ])) == 0) {
+  data$norm <- FALSE
   print("RAW COUNTS SUPPLIED")
 } else {
+  data$norm <- TRUE
   print("NORMALIZED COUNTS SUPPLIED")
 }
+saveRDS(data, "temp/data.rds")
 
 seurat[["RNA"]]@counts[1:5,1:5]
 dplyr::glimpse(seurat@meta.data)
+
+saveRDS(seurat, "temp/raw.rds")

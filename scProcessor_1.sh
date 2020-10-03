9e9dedde00c485cd36a4eabe7662391e51c0e5c5
GLANL <jordi.camps@bayer.com>
2020-10-03 15:27:56 +0200

    removed norm

--- scProcessor_1.sh
+++ scProcessor_1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 SCRIPT='/gpfs01/home/glanl/scripts/IMMUcan/scProcessor_1.R'
 BATCH='patient'
-NORM='FALSE'
 GENES='250'
 MITO='15'
 PCA='20'
 
 ml R/3.6.3-foss-2016b
 
-srun --mem=100G --time=4:00:00 --cpus-per-task=5 --partition=bigmem Rscript ${SCRIPT} ${BATCH} ${NORM} ${GENES} ${MITO} ${PCA}
+srun --mem=100G --time=4:00:00 --cpus-per-task=5 --partition=bigmem Rscript ${SCRIPT} ${BATCH} ${GENES} ${MITO} ${PCA}

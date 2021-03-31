f2a57e6f97e27fbda5618a6c4ed7e6ba457c6257
GLANL <jordi.camps@bayer.com>
2021-03-31 08:32:18 +0000

    Print top 10 genes for a cluster

--- annotate.R
+++ annotate.R
@@ -12,7 +12,11 @@ if (file.exists("out/annotation_copy.xlsx")) {
 annotation <- editData(annotation)
 
 # Use DE for checking top genes of some clusters
-DE_genes <- read.csv("temp/DE_genes.csv")
+DE_genes <- read.csv("temp/DE_genes.csv", row.names = TRUE)
+
+#Check top 10 genes of a certain cluster
+clust = 19
+head(DE_genes[DE_genes$cluster == clust, ], 10)
 
 write.xlsx(annotation, "out/annotation.xlsx")
 if(file.exists("out/annotation_copy.xlsx")) {file.remove("out/annotation_copy.xlsx")}

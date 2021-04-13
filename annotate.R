0759fc7b4d3ee2d8a186455a452450b64129825b
GLANL <jordi.camps@bayer.com>
2021-04-13 09:32:37 +0000

    small fix

--- annotate.R
+++ annotate.R
@@ -2,6 +2,7 @@ library(readxl)
 library(editData)
 library(openxlsx)
 
+#Load annotation.xlsx
 if (file.exists("out/annotation_copy.xlsx")) {
   print("Reading annotation_copy.xlsx")
   annotation <- read_excel("out/annotation_copy.xlsx")
@@ -9,13 +10,14 @@ if (file.exists("out/annotation_copy.xlsx")) {
   annotation <- read_excel("out/annotation.xlsx")
 }
 
+# Use DE for checking top genes of some clusters
+DE_genes <- read.csv("temp/DE_genes.csv", row.names = 1)
+
+# Change annotation
 annotation <- editData(annotation)
 
-# Use DE for checking top genes of some clusters
-DE_genes <- read.csv("temp/DE_genes.csv", row.names = TRUE)
-
 #Check top 10 genes of a certain cluster
-clust = 19
+clust = 1
 head(DE_genes[DE_genes$cluster == clust, ], 10)
 
 write.xlsx(annotation, "out/annotation.xlsx")
